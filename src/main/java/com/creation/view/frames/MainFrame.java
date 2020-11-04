6e8a0afb491992e4b1d4663ae131055e321d2331
AERO <tvadmix@gmail.com>
2020-11-04 17:43:32 +0300

    Frame profile

--- src/main/java/com/creation/view/frames/MainFrame.java
+++ src/main/java/com/creation/view/frames/MainFrame.java
@@ -1,16 +1,13 @@
 package com.creation.view.frames;
 
-import com.creation.entity.Product;
 import com.creation.view.elements.HTextField;
-import com.creation.view.elements.row.RowProductPanel;
-import com.creation.view.elements.row.creators.RowProductCreator;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
     private JButton button1;
-    private JButton button2;
+    boolean isFilterClicked = true;
     private JButton button3;
     private JPanel panel1;
     private JSplitPane splitPane1;
@@ -22,8 +19,14 @@ public class MainFrame extends JFrame {
     private JLabel searchByLabel;
     private JComboBox searchByBox;
     private JPanel filterPanel1;
+    private JButton ApplyFilterProdButton;
+    private JTextField СostFromTextF;
+    private JPanel filterPanelProduct;
+    private JTextField CostToTextF;
+    private JPanel FullPanel;
+    private JComboBox CategoryProductBox;
 
-    public MainFrame(){
+    public MainFrame() {
         setTitle("Market Place");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setContentPane(mainPanel);
@@ -35,7 +38,7 @@ public class MainFrame extends JFrame {
         splitPane1 = new JSplitPane();
         panel1 = new JPanel();
         button1 = new JButton("Hello");
-        button2 = new JButton("Hi");
+        ApplyFilterProdButton = new JButton("Hi");
         button3 = new JButton("Buy");
         searchButton = new JButton();
         searchField = new HTextField("Поиск...");
@@ -43,9 +46,31 @@ public class MainFrame extends JFrame {
         searchPanel = new JPanel();
         filterPanel1 = new JPanel();
         searchByLabel = new JLabel("Поиск по");
-        searchByBox = new JComboBox();
+        searchByBox = new JComboBox<String>();
         searchByBox.addItem("По товарам");
         searchByBox.addItem("По магазинам");
+        filterPanelProduct = new JPanel();
+        СostFromTextF = new HTextField("Стоимость от");
+        CostToTextF = new HTextField("Стоимость до");
+        CategoryProductBox = new JComboBox<String>();
+        CategoryProductBox.addItem("Верхняя одежда");
+        CategoryProductBox.addItem("Штаны");
+        CategoryProductBox.addItem("Шорты");
+        CategoryProductBox.addItem("Футболки");
+        CategoryProductBox.addItem("Украшения");
+        filterBut.setBackground(Color.WHITE);
+        filterBut.addActionListener((e) -> {
+            if (isFilterClicked) {
+                filterPanel1.setVisible(false);
+                filterPanelProduct.setVisible(false);
+                isFilterClicked = false;
+            } else {
+                filterPanel1.setVisible(true);
+                filterPanelProduct.setVisible(true);
+                isFilterClicked = true;
+            }
+
+        });
         /*
         Product product = new Product();
         product.setTitle("Car");
