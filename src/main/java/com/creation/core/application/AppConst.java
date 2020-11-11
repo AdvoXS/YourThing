c917912659d5c04455305642651f00cbebeb84dd
AERO <tvadmix@gmail.com>
2020-11-12 01:18:37 +0300

    Константы приложения

---
+++ src/main/java/com/creation/core/application/AppConst.java
@@ -0,0 +1,10 @@
+package com.creation.core.application;
+
+public class AppConst {
+    //Cache objects constants
+    public static final String OBJECT_AUTH = "Auth";
+
+
+    //Cache strings constants
+    public static final String USER_TOKEN = "Token";
+}
